297db41425074baa08b6cc907e7fd357de3f7b68
Aug5th <namhnt.0508@gmail.com>
2023-06-18 14:03:10 +0700

    + Add floating health text when take damage for Enemy

--- Assets/_Script/Units/EnemyBase.cs
+++ Assets/_Script/Units/EnemyBase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -7,6 +8,11 @@ public class EnemyBase : MyMonoBehaviour , IDamageable
 {
     private ObjectPool<EnemyBase> pool;
     public EnemyStats Stats { get; private set; }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+    }
     public virtual void SetStats(EnemyStats stats)
     {
         Stats = stats;
@@ -27,6 +33,9 @@ public class EnemyBase : MyMonoBehaviour , IDamageable
 
     public void ApplyDamage(float amount)
     {
+        var healthText = HealthTextSpawner.Instance.SpawnHealthText(amount.ToString(), Color.red);
+        healthText.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+
         currentHealth -= amount;
         Debug.Log("HP : " + currentHealth + " / " + Stats.Health);
         if (currentHealth <= 0)
